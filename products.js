c60a4facde8f1f0c9e1209c99cefa23da5e1db47
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2024-11-26 10:05:51 +0100

    configured es module support and put products array in separate file

---
+++ products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    id: 0,
+    name: 'Kitchen Towel',
+    price: 250,
+    rating: 5,
+    amount: 0,
+    category: 'kitchen',
+    /*img: {
+              url:,
+              alt:
+              width:,
+              height:
+          }*/
+  },
+  {
+    id: 1,
+    name: 'Bath Towel',
+    price: 375,
+    rating: 4,
+    amount: 0,
+    category: 'bathroom',
+    /*img: {
+              url:,
+              alt:
+              width:,
+              height:
+          }*/
+  },
+  {
+    id: 2,
+    name: 'Hand Towel',
+    price: 265,
+    rating: 3,
+    amount: 0,
+    category: 'bathroom',
+    /*img: {
+              url:,
+              alt:
+              width:,
+              height:
+          }*/
+  },
+  {
+    id: 3,
+    name: 'Hand Towel 2',
+    price: 275,
+    rating: 3,
+    amount: 0,
+    category: 'bathroom',
+    /*img: {
+              url:,
+              alt:
+              width:,
+              height:
+          }*/
+  },
+  {
+    id: 4,
+    name: 'Small Towel',
+    price: 175,
+    rating: 3,
+    amount: 0,
+    category: 'bathroom',
+    /*img: {
+              url:,
+              alt:
+              width:,
+              height:
+          }*/
+  },
+  {
+    id: 5,
+    name: 'Kitchen Towel 2',
+    price: 260,
+    rating: 4,
+    amount: 0,
+    category: 'kitchen',
+    /*img: {
+              url:,
+              alt:
+              width:,
+              height:
+          }*/
+  },
+];
+
+export default products;
--- script.js
+++ script.js
@@ -1,90 +1,94 @@
+import products from './products.js';
+
+console.log(products);
+
 // Create an array for all products as objects
-const products = [
-  {
-    id: 0,
-    name: 'Kitchen Towel',
-    price: 250,
-    rating: 5,
-    amount: 0,
-    category: 'kitchen',
-    /*img: {
-            url:,
-            alt:
-            width:,
-            height:
-        }*/
-  },
-  {
-    id: 1,
-    name: 'Bath Towel',
-    price: 375,
-    rating: 4,
-    amount: 0,
-    category: 'bathroom',
-    /*img: {
-            url:,
-            alt:
-            width:,
-            height:
-        }*/
-  },
-  {
-    id: 2,
-    name: 'Hand Towel',
-    price: 265,
-    rating: 3,
-    amount: 0,
-    category: 'bathroom',
-    /*img: {
-            url:,
-            alt:
-            width:,
-            height:
-        }*/
-  },
-  {
-    id: 3,
-    name: 'Hand Towel 2',
-    price: 275,
-    rating: 3,
-    amount: 0,
-    category: 'bathroom',
-    /*img: {
-            url:,
-            alt:
-            width:,
-            height:
-        }*/
-  },
-  {
-    id: 4,
-    name: 'Small Towel',
-    price: 175,
-    rating: 3,
-    amount: 0,
-    category: 'bathroom',
-    /*img: {
-            url:,
-            alt:
-            width:,
-            height:
-        }*/
-  },
-  {
-    id: 5,
-    name: 'Kitchen Towel 2',
-    price: 260,
-    rating: 4,
-    amount: 0,
-    category: 'kitchen',
-    /*img: {
-            url:,
-            alt:
-            width:,
-            height:
-        }*/
-  },
-];
+// const products = [
+//   {
+//     id: 0,
+//     name: 'Kitchen Towel',
+//     price: 250,
+//     rating: 5,
+//     amount: 0,
+//     category: 'kitchen',
+//     /*img: {
+//             url:,
+//             alt:
+//             width:,
+//             height:
+//         }*/
+//   },
+//   {
+//     id: 1,
+//     name: 'Bath Towel',
+//     price: 375,
+//     rating: 4,
+//     amount: 0,
+//     category: 'bathroom',
+//     /*img: {
+//             url:,
+//             alt:
+//             width:,
+//             height:
+//         }*/
+//   },
+//   {
+//     id: 2,
+//     name: 'Hand Towel',
+//     price: 265,
+//     rating: 3,
+//     amount: 0,
+//     category: 'bathroom',
+//     /*img: {
+//             url:,
+//             alt:
+//             width:,
+//             height:
+//         }*/
+//   },
+//   {
+//     id: 3,
+//     name: 'Hand Towel 2',
+//     price: 275,
+//     rating: 3,
+//     amount: 0,
+//     category: 'bathroom',
+//     /*img: {
+//             url:,
+//             alt:
+//             width:,
+//             height:
+//         }*/
+//   },
+//   {
+//     id: 4,
+//     name: 'Small Towel',
+//     price: 175,
+//     rating: 3,
+//     amount: 0,
+//     category: 'bathroom',
+//     /*img: {
+//             url:,
+//             alt:
+//             width:,
+//             height:
+//         }*/
+//   },
+//   {
+//     id: 5,
+//     name: 'Kitchen Towel 2',
+//     price: 260,
+//     rating: 4,
+//     amount: 0,
+//     category: 'kitchen',
+//     /*img: {
+//             url:,
+//             alt:
+//             width:,
+//             height:
+//         }*/
+//   },
+// ];
 
 // ----------------------------
 // --- HTML ELEMENTS ----------
